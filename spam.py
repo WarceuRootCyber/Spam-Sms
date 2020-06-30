99cde0824df7094ac2a9d26f4297a3e637fa3bc6
☆LIGHT-ID☆ <66877990+WarceuRootCyber@users.noreply.github.com>
2020-06-30 23:36:21 +0700

    spam.py

--- spam.py
+++ spam.py
@@ -1,7 +1,7 @@
 import os
 os.system('clear')
 print 'Maaf, Tools Sedang Tidak Aktif'
-print 'cobalah lah memakah tools spam-sms.v1" ^_^' 
+print 'cobalah lah memakai tools spam-sms.v1" ^_^' 
 print 'copy command di bawah untuk cloning tools spam baru :v'
 print 'https://github.com/WarceuRootCyber/spam-sms.v1'
 print 'Ataw bisa langsung hubungi FB https://facebook.com/ckasepgigir.kobong'
